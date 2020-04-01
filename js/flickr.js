7d4051340fe2004609317a01455942b66288a0cb
Alan <alanrd772@gmail.com>
2020-04-01 14:11:52 +0400

    Small update

--- js/flickr.js
+++ js/flickr.js
@@ -2,9 +2,9 @@ function gallery(data) {
     for (p of data.photos.photo) {
         $('#port-items').append(`
         <div class="p-item">
-        <img src="https://farm${p.farm}.staticflickr.com/${p.server}/${p.id}_${p.secret}_z.jpg" alt="${p.title}" class="img-responsive centered"style="display: block;">
-        <div class="p-name">${p.title}</div>
-        <div class="p-desc">${p.description._content}</div>
+            <img src="https://farm${p.farm}.staticflickr.com/${p.server}/${p.id}_${p.secret}_z.jpg" alt="${p.title}" class="img-responsive centered"style="display: block;">
+            <div class="p-name">${p.title}</div>
+            <div class="p-desc">${p.description._content}</div>
         </div>
         `)
     }
