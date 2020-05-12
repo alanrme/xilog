230a5adaecd965f8bb675778b6917f920396f48a
Alan <alanrd772@gmail.com>
2020-05-12 18:21:18 +0400

    new gallery !!! viewport and a easteregg if ur a nerd

--- js/easteregg.js
+++ js/easteregg.js
@@ -1,12 +1,13 @@
 $(function(){
-    let x = $('#easteregg') // find easteregg
+    var x = $('#easteregg') // find easteregg
     $('#easteregg-content #close').click(function() { // when close button clicked
         x.fadeOut()
     })
 
     //CLICK-TRIGGER EASTEREGG
     $('#trigger').click(function() { // when trigger clicked
-        let audio = new Audio('../audio/easteregg.wav');
+        // var for compatibility
+        var audio = new Audio('../audio/easteregg.wav');
         audio.play();
         $('body').css('animation', 'shake 0.5s')
         setTimeout(function() { x.fadeIn() }, 2000);
@@ -45,10 +46,14 @@ $(function(){
             if (count == 10){
                 // Success!
                 if (x.css('display') === "none") { // if diplay none fade in
-                    let audio = new Audio('../audio/easteregg.wav');
-                    audio.play();
-                    $('body').css('animation', 'shake 0.5s')
-                    setTimeout(function() { x.fadeIn() }, 2000);
+                    var audio = new Audio('../audio/easteregg.wav'); // new audio element
+                    audio.play(); // play it
+                    // remove any leftover animation and add a shake animation to body
+                    $('body').css('animation', 'none').css('animation', 'shake 0.5s')
+                    setTimeout(function() {
+                        x.fadeIn();
+                        $('body');
+                    }, 2000);
                 } else {
                     x.fadeOut(); // vice versa
                 }
--- js/portfolio.js
+++ js/portfolio.js
@@ -1,18 +1,25 @@
 $(function(){
     // PORTFOLIO
-    let portView = $('#p-view');
-    $('#port-items .p-item').each(function ()
-    {
+    let modal = $('#p-view'); // this is the modal for the portfolio's viewer
+    $('#port-items .p-item').each(function () { // for each portfolio item
         let img = $($(this).find('img')[0]);
         let item = $(this);
         img.on('click', function () { // if image is clicked on
-            portView.empty(); // clear portview
-            item.clone().appendTo(portView); // clone this to portview, on top of p-over
-            portView.prepend('<div id="p-over"></div>'); // add a trigger behind to close
-            portView.fadeIn(); // animate portview from 0 to 1 opacity
-            $('#p-over').on('click', function () {
-                portView.fadeOut(function () { portView.empty(); })
-                // if user clicks anywhere, fade out portview then clear it
+            modal.empty(); // clear modal
+            item.clone().prependTo(modal); // clone this to modal, on top of p-over
+            modal.fadeIn(); // animate modal from 0 to 1 opacity
+
+            // if portfolio-overlay is clicked
+            modal.on('click', function () {
+                modal.fadeOut(function () { modal.empty(); })
+                // if user clicks anywhere, fade out modal then clear it
+            });
+            // if the image in the modal is clicked stop the event
+            // from propagating up to p-over's click event so you
+            // can click on a button in the image
+            $('#p-view .p-item img').click(function(event){
+                event.stopPropagation();
+                console.log("fuck")
             });
         });
     });
