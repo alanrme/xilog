89057bd9b50ba8f9c6dc676b774241dda3c7b4a9
Alan <alanrd772@gmail.com>
2021-06-27 13:53:27 +0400

    Animation edits and improved for OLED

--- js/github.js
+++ js/github.js
@@ -5,7 +5,7 @@ fetch("https://api.github.com/search/repositories?q=user:XilogOfficial&sort=star
         item = data.items[i]
         $(".gh-projects").append(`
         <a href="${item.html_url}" style="color: inherit;">
-            <div class="card">
+            <div class="card aos" data-aos="fadein-up">
                 <h4>${item.name}</h4>
                 <p2>${item.description}</p2>
             </div>
