957d63f45a6235c6de31dbef35745de00ecdb64c
Alan <alanrd772@gmail.com>
2022-08-29 19:38:44 +0400

    Add support for passive listeners

--- js/main.js
+++ js/main.js
@@ -8,23 +8,39 @@ window.onload = () => {
         loadBg.style.display = 'none';
         //_('#loader').style.display = 'none';
     });
-
+    
     // animate background zoom in - doesn't work right with parallax scroll so disabled
     // window.setTimeout(() => { $("#background").css("transform", "scale(1.3)"); }, 350);
 }
 
 ready(() => {
     window.scrollTo(0, 0); // scroll to top on page load
-
+    
     // set vh property to the true viewport height to fix it on mobile browsers
     document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
-
-
+    
+    
     // DISABLE RIGHT CLICK
     noRightClick = _(".norightclick", true)
     for (var i = 0; i < noRightClick.length; i++) {
         noRightClick[i].addEventListener('contextmenu', event => event.preventDefault());
     }
+    
+    
+    
+    
+    // see if the browser supports passive listeners for some events - improves performance
+    // test via a getter in the options object to see if the passive property is accessed
+    var supportsPassive = false;
+    try {
+        var opts = Object.defineProperty({}, 'passive', {
+            get: function() {
+                supportsPassive = true;
+            }
+        });
+        window.addEventListener("testPassive", null, opts);
+        window.removeEventListener("testPassive", null, opts);
+    } catch (e) {}
 
 
     // Haptics when link/button is pressed or released
@@ -33,29 +49,14 @@ ready(() => {
     addHaptics = (element) => {
         // for each element in the array, add an event listener of the same name
         ["touchstart", "touchend"].forEach(event => {
-            element.addEventListener(event, () => navigator.vibrate(5));
+            // stuff at the end is to specify the listener is passive for browsers that support it
+            element.addEventListener(event, () => navigator.vibrate(5), supportsPassive ? { passive: true } : false);
         });
     }
     _("button, a, nav .menu", true).forEach(e => addHaptics(e));
-
-
-    // DARK MODE
-    // if localstorage says user's last setting is dark
-    /*
-    darkToggle = _("#darkmode");
-    if ((localStorage.getItem('mode') || 'dark') === 'dark') {
-        document.body.classList.add('dark'); // make the page dark
-        darkToggle.checked = true; // check darkmode box
-    }
-    darkToggle.addEventListener("click", () => { // if dark/light toggle is clicked, set it in localstorage
-        localStorage.setItem('mode', (localStorage.getItem('mode') || 'dark') === 'dark' ? 'light' : 'dark');
-        // then change it on the page too, add a "dark" class to body
-        localStorage.getItem('mode') === 'dark' ? document.body.classList.add('dark') : document.body.classList.remove('dark')
-    })
-    */
     
-
-
+    
+    
     //MOBILE MENU
     menuBg = _('#menu-bg')
     _('.menu').addEventListener("click", () => {
@@ -67,11 +68,11 @@ ready(() => {
                 _('#menu').appendChild(e);
             }
         }
-
+        
         // fade in menu - delay because if it wasn't there the opacity transition didn't work
         menuBg.style.display = "flex";
         setTimeout(() => { menuBg.style.opacity = 1; }, 2);
-
+        
         // apply blur effect
         ["blur", "blur2"].forEach(id => {
             blurModal = _(`.modal#${id}`);
@@ -79,28 +80,28 @@ ready(() => {
             blurModal.classList.add("animate");
         });
     });
-
+    
     _('.m-close, #menu-bg', true).forEach(e => {
         func = () => {
             // hide both menuBg and blurModal
             menuBg.style.display = "none";
             ["blur", "blur2"].forEach(id => { _(`.modal#${id}`).classList.remove("show"); });
-
+            
             // empty the menu except for close button & remove listener so it won't fire on fadein
             _("#menu a:not(.m-close)", true).forEach(e => e.remove());
             menuBg.removeEventListener('transitionend', func);
         }
-
+        
         e.addEventListener("click", () => { // when close button clicked
             menuBg.style.opacity = 0;
             menuBg.addEventListener('transitionend', func, false);
-
+            
             // un-animate blur effect, finish setting display: none in
             // menuBg's transitionend event
             ["blur", "blur2"].forEach(id => { _(`.modal#${id}`).classList.remove("animate"); });
         })
     })
-
+    
     
     // SCROLL UP BUTTON
     // scrollup looks like it's defined to only be used once but it is
@@ -113,7 +114,7 @@ ready(() => {
             behavior: 'smooth' // smooth scroll
         });
     })
-
+    
     
     // Checking if element is in viewport (used for parallax scroll)
     function isInViewport(node) {
@@ -124,67 +125,67 @@ ready(() => {
             rect.right >= 0 &&
             rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
             rect.left <= (window.innerWidth || document.documentElement.clientWidth)
-        )
-    }
-
-
-    // SCROLL POSITION
-    let scrollpos = 0;
-    document.addEventListener('scroll', () => {
-        scrollpos = window.scrollY;
-
-        // parallax scroll
-        /*
-        $('.parallax').each(function(index, element) {
-            // Check if the element is in the viewport.
-            if (isInViewport(this)) {
-                var diff = scrollpos - this.offsetTop
-                var ratio = Math.round((diff / this.offsetHeight) * 350)
-                $(this).css('background-position','center ' + parseInt(-ratio) + 'px')
-                $(".hero").css('transform', `translateY(${parseInt(ratio*0.5)}px)`)
-            }
-        })
-        */
-    });
-
-
-
-    let intro = _('.content').offsetTop; // top of content
-    let nav = _('nav');
-    // run every 150ms, put most scroll events here
-    // more efficient than the scroll event
-    window.setInterval(function(){
-        intro = _('.content').offsetTop; // set top of content
-        // ^ this is in a loop so that when the screen is turned it
-        // will update with the new position
-
-        // if user scrolls below intro, show button
-        if (scrollpos > intro) {
-            scrollup.classList.add("show")
-        } else {
-            scrollup.classList.remove("show")
+            )
         }
-
-
-        // FIXED NAV
-        // handles attaching nav to screen when scrolled far enough
-        if (scrollpos > 200) { // after the nav is no longer visible
-            if (!nav.classList.contains('scrolled')) nav.classList.add('scrolled');
-
-            // nest these since realistically they only run if the
-            // scroll is above 100, more efficient code
-            if (scrollpos > intro) {
-                if (!nav.classList.contains('awake')) nav.classList.add('awake');
-            }
-            if (scrollpos < intro) {
-                if (nav.classList.contains('awake')) {
-                    nav.classList.remove('awake');
-                    nav.classList.add('sleep');
+        
+        
+        // SCROLL POSITION
+        let scrollpos = 0;
+        document.addEventListener('scroll', () => {
+            scrollpos = window.scrollY;
+            
+            // parallax scroll
+            /*
+            $('.parallax').each(function(index, element) {
+                // Check if the element is in the viewport.
+                if (isInViewport(this)) {
+                    var diff = scrollpos - this.offsetTop
+                    var ratio = Math.round((diff / this.offsetHeight) * 350)
+                    $(this).css('background-position','center ' + parseInt(-ratio) + 'px')
+                    $(".hero").css('transform', `translateY(${parseInt(ratio*0.5)}px)`)
                 }
+            })
+            */
+        });
+        
+        
+        
+        let intro = _('.content').offsetTop; // top of content
+        let nav = _('nav');
+        // run every 150ms, put most scroll events here
+        // more efficient than the scroll event
+        window.setInterval(function(){
+            intro = _('.content').offsetTop; // set top of content
+            // ^ this is in a loop so that when the screen is turned it
+            // will update with the new position
+            
+            // if user scrolls below intro, show button
+            if (scrollpos > intro) {
+                scrollup.classList.add("show")
+            } else {
+                scrollup.classList.remove("show")
             }
-        } 
-        if (scrollpos < 200) {
-            if (nav.classList.contains('scrolled')) nav.classList.remove("scrolled", "sleep");
-        }
-    }, 150);
-});
+            
+            
+            // FIXED NAV
+            // handles attaching nav to screen when scrolled far enough
+            if (scrollpos > 200) { // after the nav is no longer visible
+                if (!nav.classList.contains('scrolled')) nav.classList.add('scrolled');
+                
+                // nest these since realistically they only run if the
+                // scroll is above 100, more efficient code
+                if (scrollpos > intro) {
+                    if (!nav.classList.contains('awake')) nav.classList.add('awake');
+                }
+                if (scrollpos < intro) {
+                    if (nav.classList.contains('awake')) {
+                        nav.classList.remove('awake');
+                        nav.classList.add('sleep');
+                    }
+                }
+            } 
+            if (scrollpos < 200) {
+                if (nav.classList.contains('scrolled')) nav.classList.remove("scrolled", "sleep");
+            }
+        }, 150);
+    });
